6d7d88c8a5aabd2963a1f177f17105cff2296fe2
perfume-bin <68043048+perfume-bin@users.noreply.github.com>
2022-06-01 20:58:08 +0800

    Add files via upload

---
+++ lianjia/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+"""
+@author:time-无产者
+@file:__init__.py
+@time:2022/04/26
+"""
+
+if __name__ == '__main__':
+    pass
+
---
+++ lianjia/草稿纸.py
@@ -0,0 +1,22 @@
+
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+"""
+@author:time-无产者
+@file:草稿纸.py
+@time:2022/01/25
+"""
+import time
+
+a = 1
+try:
+    if a :
+        print(a+2)
+except:
+    print('b')
+    time.sleep(1)
+print('done')
+
+if __name__ == '__main__':
+    pass
+
